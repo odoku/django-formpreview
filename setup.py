662b5fcf264b71632b1b081e6c71dbae1f60a6c4
odoku <m.onogawa@gmail.com>
2014-02-15 01:24:58 +0900

    pip installに対応しました。

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-formpreview',
+    version='0.0.1',
+    description='Previewable class based form views for Django.',
+    author='odoku',
+    author_email='contact@odoku.net',
+    keywords='django,form,preview',
+    url='http://odoku.net',
+    packages=find_packages(),
+)
